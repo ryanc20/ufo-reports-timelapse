64029decc1cdf2f08ad39d5e94fb0c476b332a59
Ryan Christensen <ryan.christensen20@gmail.com>
2017-09-24 20:09:34 -0700

    Updated files to show the year and title

--- create_timelapse.py
+++ create_timelapse.py
@@ -1,4 +1,7 @@
 import imageio
+from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw
 
 def create_timelapse(first_year, last_year):
 	"""Creates a timelapse of all of the cropped images."""
@@ -7,11 +10,22 @@ def create_timelapse(first_year, last_year):
 	for file in range(first_year, last_year + 1):
 		try:
 			image = file_location + str(file) + ".png"
-			images.append(imageio.imread(image))
+			labeled_image = add_timestamp(image, file)
+			images.append(imageio.imread(labeled_image))
 		except:
-			print("File not found: " + file_location + str(file) + ".png")
+			print("File not found: " + file_location + "with_year/" + str(file) + ".png")
 	
-	imageio.mimsave('ufo_report_timelapse.gif', images)
+	imageio.mimsave('ufo_report_timelapse.gif', images, fps=5)
 	print("The files that were found were added to the GIF.")
 
-create_timelapse(1950,2014)
+def add_timestamp(image, year):
+	"""Adds the current year to the bottom right of the image for each frame."""
+	img = Image.open(image)
+	draw = ImageDraw.Draw(img)
+	font =ImageFont.truetype("font/OpenSans-Regular.ttf", 16)
+	draw.text((575, 300), str(year), (0,0,0), font=font)
+	draw.text((220, 13), "UFO Reported Sightings 1950-2014", (0,0,0), font=font)
+	img.save('images/cropped_images/with_year/' + str(year) + '.png')
+	return 'images/cropped_images/with_year/' + str(year) + '.png'
+	
+create_timelapse(1950, 2014)
